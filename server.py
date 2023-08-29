acda9bd51271f8b66b4aaf3e2141f340453b8166
Jason Chung <125996156+gitgotgod@users.noreply.github.com>
2023-08-29 15:18:04 +0900

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-from flask_app.controllers import users #controllers go here
-
-if __name__=="__main__":   
-    app.run(debug=True, host="localhost", port=8000)
-
-
-# debug needs to be set to False when deployed.
-# We shared a video showing how the information leaked by this feature and help hackers.
